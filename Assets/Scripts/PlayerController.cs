b35f00dca1d668334590ea2e060d2afc68d22cca
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-01-14 18:38:26 -0800

    player controller update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject playerTarget;
 
+    public GameObject playerModel;
+
     [Range(0.0f, 10.0f)]
     public float speed, jumpSpeed;
 
@@ -18,6 +20,8 @@ public class PlayerController : MonoBehaviour {
     private CharacterController _controller;
 
     private Vector3 _moveDirection = Vector3.zero;
+
+    private Vector3 _target;
 
     // Start is called before the first frame update
     void Start() {
@@ -34,15 +38,22 @@ public class PlayerController : MonoBehaviour {
             // Get input directions
             _moveDirection = new Vector3(Input.GetAxis("LS_h"), 0.0f, Input.GetAxis("LS_v"));
 
-            // if (Input.GetAxis("LS_h") > 0) {
+            if (Input.GetAxis("LS_h") < 0) {
 
-            //     // Face player to the right
+                // Face player model to the left
+                playerModel.transform.localEulerAngles = new Vector3(0.0f, transform.rotation.y - 90.0f, 0.0f);
 
-            // } else if (Input.GetAxis("LS_h") < 0) {
+            } else if (Input.GetAxis("LS_h") > 0) {
 
-            //     // Face player to the left
+                // Face player model to the right
+                playerModel.transform.localEulerAngles = new Vector3(0.0f, transform.rotation.y + 90.0f, 0.0f);
 
-            // }
+            } else {
+
+                // Face player model outwards towards camera
+                playerModel.transform.localEulerAngles = new Vector3(0.0f, transform.rotation.y + 180.0f, 0.0f);
+
+            }
 
             // Maintains direction after movement stops
             _moveDirection = transform.TransformDirection(_moveDirection);
@@ -74,12 +85,12 @@ public class PlayerController : MonoBehaviour {
         // Move the Controller
         _controller.Move(_moveDirection * Time.deltaTime);
 
-        Vector3 target = new Vector3(playerTarget.transform.position.x,
+        _target = new Vector3(playerTarget.transform.position.x,
                                      this.transform.position.y,
                                      playerTarget.transform.position.z);
 
         // face the player towards the target
-        transform.LookAt(target);
+        transform.LookAt(_target);
 
     }
 }
