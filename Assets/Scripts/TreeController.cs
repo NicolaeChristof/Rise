47fb092ed79e02830f4b521fde3cd7745d99bd67
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-01-16 13:10:05 -0800

    Implement reticle script

--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeController : MonoBehaviour
-{
+public class TreeController : MonoBehaviour {
     // Local References
     private GameObject _tree;
     private GameObject _reticle;
@@ -12,11 +11,12 @@ public class TreeController : MonoBehaviour
     private const string MOVE_VERTICAL = "LS_v";
     private const string MOVE_LATERAL = "LS_h";
     private const string GROW = "LB";
-    private const float SPEED = 2.15F;
+    private const float VERTICAL_SPEED = 2.15F;
+    private const float LATERAL_SPEED = 6.30F;
     private const float EPSILON = 0.01F;
+    private const float DISTANCE = 2.0F;
 
-    void Start()
-    {
+    void Start() {
         // Establish local references
         _tree = GameObject.Find("Tree");
         _reticle = GameObject.Find("Tree Reticle");
@@ -26,25 +26,56 @@ public class TreeController : MonoBehaviour
         // Poll Input
         float moveVertical = Input.GetAxis(MOVE_VERTICAL);
         float moveLateral = Input.GetAxis(MOVE_LATERAL);
+        float grow = Input.GetAxis(GROW);
 
         bool moved = false;
 
         // If vertical axis is actuated beyond epsilon value, translate reticle vertically
-        if (System.Math.Abs(moveVertical) > EPSILON) {
-            transform.Translate(Vector3.up * (moveVertical * Time.deltaTime * SPEED) * -1, Space.World);
+        if (CheckEpsilon(moveVertical)) {
+            transform.Translate(Vector3.up * (moveVertical * Time.deltaTime * VERTICAL_SPEED) * -1, Space.World);
             moved = true;
         }
 
         // If horizontal axis is actuated beyond epsilon value, translate reticle horizontally
-        if (System.Math.Abs(moveLateral) > EPSILON) {
-            transform.Translate(Vector3.right * (moveLateral * Time.deltaTime * SPEED), Space.Self);
+        if (CheckEpsilon(moveLateral)) {
+            transform.Translate(Vector3.right * (moveLateral * Time.deltaTime * LATERAL_SPEED), Space.Self);
             moved = true;
         }
 
         // If the controller was actuated, move the reticle object
         if (moved) {
-            print(moveVertical);
-            print(moveLateral);
+            UpdateReticle();
         }
+
+        if (CheckEpsilon(grow)) {
+            OnGrow();
+        }
+    }
+
+    private void OnGrow() {
+        // TODO!
+    }
+
+    private void UpdateReticle() {
+        // Horizontal-only raycast
+        Vector3 position = _tree.transform.position;
+        position.y = transform.position.y;
+        Physics.Linecast(transform.position, position, out RaycastHit raycast);
+
+        // Resolve Position
+        transform.position = ((transform.position - raycast.point).normalized * DISTANCE) + raycast.point;
+
+        // Resolve Facing
+        transform.LookAt(_tree.transform.position);
+
+        // Resolve Reticle Position
+        _reticle.transform.position = raycast.point;
+
+        // Resolve Reticle Facing
+        _reticle.transform.LookAt(raycast.point + (raycast.normal * 2.0F));
+    }
+
+    private bool CheckEpsilon(float passedValue) {
+        return (System.Math.Abs(passedValue) > EPSILON);
     }
 }
