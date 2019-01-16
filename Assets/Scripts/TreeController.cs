3ee7c1c8271f60d99a554043ef8e3de0acf8d28d
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-01-16 12:10:43 -0800

    Test inputs

--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -12,7 +12,6 @@ public class TreeController : MonoBehaviour
     private const string MOVE_VERTICAL = "DPAD_v";
     private const string MOVE_LATERAL = "DPAD_h";
     private const string GROW = "LB";
-
     private const float SPEED = 2.15F;
     private const float EPSILON = 0.01F;
 
@@ -23,8 +22,29 @@ public class TreeController : MonoBehaviour
         _reticle = GameObject.Find("Tree Reticle");
     }
 
-    void Update()
-    {
+    void Update() {
+        // Poll Input
+        float moveVertical = Input.GetAxis(MOVE_VERTICAL);
+        float moveLateral = Input.GetAxis(MOVE_LATERAL);
 
+        bool moved = false;
+
+        // If vertical axis is actuated beyond epsilon value, translate reticle vertically
+        if (System.Math.Abs(moveVertical) > EPSILON) {
+            transform.Translate(Vector3.up * (moveVertical * Time.deltaTime * SPEED) * -1, Space.World);
+            moved = true;
+        }
+
+        // If horizontal axis is actuated beyond epsilon value, translate reticle horizontally
+        if (System.Math.Abs(moveLateral) > EPSILON) {
+            transform.Translate(Vector3.right * (moveLateral * Time.deltaTime * SPEED), Space.Self);
+            moved = true;
+        }
+
+        // If the controller was actuated, move the reticle object
+        if (moved) {
+            print(moveVertical);
+            print(moveLateral);
+        }
     }
 }
