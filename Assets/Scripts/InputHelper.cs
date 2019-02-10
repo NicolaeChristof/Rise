f30688369909e2c33c72fc9e9feff61cd2e3bdb8
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-02-09 20:38:49 -0800

    Add enums for controls

--- Assets/Scripts/InputHelper.cs
+++ Assets/Scripts/InputHelper.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHelper : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class InputHelper : MonoBehaviour {
+	// Public Fields
+	public enum SquirrelInput {
+		MOVE_HORIZONTAL,
+		MOVE_VERTICAL,
+		SWAP,
+		PAUSE,
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		JUMP,
+		PEEK_LEFT,
+		PEEK_RIGHT,
+	}
+
+	public enum TreeInput {
+		MOVE_HORIZTONAL,
+		MOVE_VERTICAL,
+		SWAP,
+		PAUSE,
+
+		BRANCH_PLACE,
+		BRANCH_REMOVE,
+		SELECT_LEFT,
+		SELECT_RIGHT,
+	}
+
+	public InputHelper() {
+
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
 }
