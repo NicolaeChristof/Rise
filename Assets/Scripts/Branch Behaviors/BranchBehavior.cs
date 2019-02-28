17b453c9f31075f9d2dc801b2bcef64d2664b274
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-02-27 18:04:27 -0800

    polished tween effect

--- Assets/Scripts/Branch Behaviors/BranchBehavior.cs
+++ Assets/Scripts/Branch Behaviors/BranchBehavior.cs
@@ -5,60 +5,62 @@ using DG.Tweening;
 
 public class BranchBehavior : MonoBehaviour {
 
-	// Public Fields
-	public string readableName = "Normal Branch";
-	public GameObject knot;
+    // Public Fields
+    public string readableName = "Normal Branch";
+    public GameObject knot;
 
     // Private fields
-    private Transform restingPosition;
+    private Vector3 _originalRotation;
 
     private float _deformationAngle;
 
-	void Start() {
+    void Start() {
 
-        restingPosition = transform;
+        _originalRotation = transform.localEulerAngles;
 
         _deformationAngle = 8.0f;
 
-	}
+    }
 
-	void Update() {
+    void Update() {
 
-	}
 
-	public virtual void OnTriggerEnter(Collider collider) {
 
-        if (collider.gameObject.name == "Squirrel") {
+    }
 
-            transform.DORotate(transform.localEulerAngles + Quaternion.AngleAxis(_deformationAngle, Vector3.right).eulerAngles, 0.2f);
-
-            // transform.DORotate(transform.localEulerAngles + Quaternion.AngleAxis(-25, Vector3.right).eulerAngles, 0.5f);
-
-        }
-
-	}
-
-	public virtual void OnTriggerStay(Collider collider) {
-
-	}
-
-	public virtual void OnTriggerExit(Collider collider) {
+    public virtual void OnTriggerEnter(Collider collider) {
 
         if (collider.gameObject.name == "Squirrel") {
 
-            transform.DORotate(transform.localEulerAngles + Quaternion.AngleAxis(-_deformationAngle, Vector3.right).eulerAngles, 0.2f);
+            transform.DORotate(transform.localEulerAngles + Quaternion.AngleAxis(_deformationAngle, Vector3.right).eulerAngles, 2.0f, RotateMode.Fast)
+                .SetEase(Ease.OutElastic);
 
         }
 
-	}
+    }
 
-	public virtual void OnBreak() {
-		// TODO: Play break sound
-		Instantiate(knot, transform.position, transform.rotation);
-	}
+    public virtual void OnTriggerStay(Collider collider) {
 
-	// TODO: Internalize? I18n?
-	public virtual string GetReadableName() {
-		return readableName;
-	}
+    }
+
+    public virtual void OnTriggerExit(Collider collider) {
+
+        if (collider.gameObject.name == "Squirrel") {
+
+            transform.DORotate(_originalRotation, 2.0f, RotateMode.Fast)
+                .SetEase(Ease.OutElastic);
+
+        }
+
+    }
+
+    public virtual void OnBreak() {
+        // TODO: Play break sound
+        Instantiate(knot, transform.position, transform.rotation);
+    }
+
+    // TODO: Internalize? I18n?
+    public virtual string GetReadableName() {
+        return readableName;
+    }
 }
