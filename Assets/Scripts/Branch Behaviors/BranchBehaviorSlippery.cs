e09da3bd9cc9c6ce5313f4a7b55b7b7d5fd0320b
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-03-10 16:27:01 -0700

    slippery branch implemented

--- Assets/Scripts/Branch Behaviors/BranchBehaviorSlippery.cs
+++ Assets/Scripts/Branch Behaviors/BranchBehaviorSlippery.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class BranchBehaviorSlippery : BranchBehavior {
 
-	// Public Fields
-	public float slipFactor = 0.1F;
+    // Public Fields
+    public float slipForce;
 
-	public override void OnTriggerEnter (Collider collider) {
+    // Private Fields
+    private Vector3 _slipDirection;
+
+    public override void OnTriggerEnter (Collider collider) {
 
         base.OnTriggerEnter(collider);
 
-		if (collider.gameObject.tag.Equals("Player")) {
-			// TODO: Make it slippery using only instantaneous velocity! Wheee!
-		}
-	}
+        if (collider.gameObject.tag.Equals("Player")) {
+
+            if (Random.Range(-1.0f, 1.0f) > 0) {
+
+                _slipDirection = new Vector3(slipForce, 0.0f, 0.0f);
+
+            } else {
+
+                _slipDirection = new Vector3(-slipForce, 0.0f, 0.0f);
+
+            }
+
+            Debug.Log(_slipDirection);
+
+            collider.gameObject.GetComponent<PlayerController>().stunPlayer(0.25f);
+
+            collider.gameObject.GetComponent<PlayerController>().addExternalForce(_slipDirection);
+
+        }
+    }
 
     public override void OnTriggerStay (Collider collider) {
 
@@ -22,12 +41,9 @@ public class BranchBehaviorSlippery : BranchBehavior {
 
     }
 
-	public override void OnTriggerExit (Collider collider) {
+    public override void OnTriggerExit (Collider collider) {
 
         base.OnTriggerExit(collider);
 
-		if (collider.gameObject.tag.Equals("Player")) {
-			// TODO: Make it slippery using only instantaneous velocity! Wheee!
-		}
-	}
+    }
 }
