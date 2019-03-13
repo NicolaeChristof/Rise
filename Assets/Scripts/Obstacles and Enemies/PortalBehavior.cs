4292fa968b4f23271e39d79ce4fe6b1a8a1d9f63
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-03-13 12:39:37 -0700

    portal funcitonal

--- Assets/Scripts/Obstacles and Enemies/PortalBehavior.cs
+++ Assets/Scripts/Obstacles and Enemies/PortalBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RiseExtensions;
 
 public class PortalBehavior : MonoBehaviour {
 
@@ -17,8 +18,12 @@ public class PortalBehavior : MonoBehaviour {
     // Private Fields
     private Vector3 _target;
 
+    private bool _portalActive = true;
+
     // Start is called before the first frame update
     void Start() {
+
+        _portalExit = exitPortal.transform.GetChild(0).gameObject;
 
         _tree = GameObject.FindGameObjectWithTag("Tree");
 
@@ -39,7 +44,9 @@ public class PortalBehavior : MonoBehaviour {
 
         if (collider.gameObject.tag.Equals("Player")) {
 
-            Debug.Log("hello");
+            // exitPortal.GetComponent<PortalBehavior>().ActivatePortal();
+
+            // _portalActive = true;
 
         }
 
@@ -47,13 +54,48 @@ public class PortalBehavior : MonoBehaviour {
 
     void OnTriggerStay (Collider collider) {
 
+        if (collider.gameObject.tag.Equals("Player")) {
 
+            if (InputHelper.GetButtonDown(SquirrelInput.ACTION_1) && _portalActive) {
+
+                _portalActive = false;
+
+                exitPortal.GetComponent<PortalBehavior>().DeactivatePortal();
+
+                Debug.Log(this.name + " my location " + transform.position + " exit location " + _portalExit.transform.position);
+
+                collider.gameObject.transform.position = _portalExit.transform.position;
+
+                // exitPortal.GetComponent<PortalBehavior>().ActivatePortal();
+
+                // _portalActive = true;
+
+            }
+
+        }
 
     }
 
     void OnTriggerExit (Collider collider) {
 
+        if (collider.gameObject.tag.Equals("Player")) {
 
+            _portalActive = true;
+
+        }
 
     }
+
+    public void ActivatePortal () {
+
+        _portalActive = true;
+
+    }
+
+    public void DeactivatePortal () {
+
+        _portalActive = false;
+
+    }
+
 }
