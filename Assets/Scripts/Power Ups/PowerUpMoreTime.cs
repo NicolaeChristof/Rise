7b179ab097925c36d9d82dff18a8a31ed8831a92
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-03-13 17:48:10 -0700

    updated powerup prefab

--- Assets/Scripts/Power Ups/PowerUpMoreTime.cs
+++ Assets/Scripts/Power Ups/PowerUpMoreTime.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class PowerUpMoreTime : MonoBehaviour
-{
+public class PowerUpMoreTime : MonoBehaviour {
+
+    // Public References
+    public AudioClip collectSound;
+
+    // Public Fields
+
+    // Private References
+    private AudioSource _source;
+
+    // Private Fields
+    private Vector3 _newScale;
+
+    private bool _canCollect = true;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+
+        _source = GetComponent<AudioSource>();
+
+        _newScale = Vector3.zero;
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    void OnTriggerEnter (Collider collider) {
+
+        if (collider.gameObject.tag.Equals("Player") && _canCollect) {
+
+            _canCollect = false;
+
+            GameModel.timer += 20.0f;
+
+            float _volume = Random.Range(GameModel.volLowRange, GameModel.volHighRange);
+            _source.PlayOneShot(collectSound, _volume);
+
+            transform.DOScale(_newScale, 0.75f)
+                .OnComplete(()=>Destroy(this));
+
+        }
+
+    }
+
+    void OnTriggerStay (Collider collider) {
+
+
+
+    }
+
+    void OnTriggerExit (Collider collider) {
+
+
+
     }
 }
