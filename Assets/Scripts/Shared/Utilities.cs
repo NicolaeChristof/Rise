506516e5e5c6757d01756ae8669cb24742b75259
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-03-14 22:30:11 -0700

    Add Utilities static class
    We have RiseExtensions namespace, we might as well use it
    
    - Adds Clamp extension method for IComparable
    - Adds CheckEpsilon extension method for float and double

---
+++ Assets/Scripts/Shared/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RiseExtensions {
+
+    public static class MathUtilities {
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T> {
+            if (val.CompareTo(min) < 0) return min;
+            if (val.CompareTo(max) > 0) return max;
+            return val;
+        }
+
+        public static bool CheckEpsilon(this float val, float epsilon) {
+            return Math.Abs(val) > 0;
+        }
+
+        public static bool CheckEpsilon(this double val, double epsilon) {
+            return Math.Abs(val) > 0;
+        }
+    }
+
+}
