6a3cb99ece63f4ad9b4287bdf321cb0696c3f718
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-03-14 23:45:49 -0700

    Refactor SapType to SapBehavior

---
+++ Assets/Scripts/SapBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using RiseExtensions;
+
+public class SapBehavior : RiseBehavior {
+
+    // Public Fields
+    public AudioClip pickupSound;
+    public SapType sapType;
+    public int sapValue = 1;
+    public bool canCollect = true;
+
+    public override void UpdateAlways() {
+
+    }
+
+    public override void UpdateTick() {
+
+    }
+
+    public virtual void OnSapCollected() {
+        transform.DOScale(Vector3.zero, 0.75f).OnComplete(() => Destroy(this));
+    }
+}
