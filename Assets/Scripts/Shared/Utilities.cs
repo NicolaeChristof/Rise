a339a4737854aa3065e4e0f49dc35a3df53fe305
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-03-14 22:52:12 -0700

    Add comments to Utilities

--- Assets/Scripts/Shared/Utilities.cs
+++ Assets/Scripts/Shared/Utilities.cs
@@ -3,16 +3,36 @@
 namespace RiseExtensions {
 
     public static class MathUtilities {
+
+        /// <summary>
+        /// Clamp the specified value to be between the minimum and maximum values.
+        /// </summary>
+        /// <returns>The clamped value.</returns>
+        /// <param name="val">Value.</param>
+        /// <param name="min">Minimum.</param>
+        /// <param name="max">Max.</param>
         public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T> {
             if (val.CompareTo(min) < 0) return min;
             if (val.CompareTo(max) > 0) return max;
             return val;
         }
 
+        /// <summary>
+        /// Checks whether the magnitude of the float value is greater than the passed epsilon value.
+        /// </summary>
+        /// <returns><c>true</c>, if epsilon check passes, <c>false</c> otherwise.</returns>
+        /// <param name="val">Value.</param>
+        /// <param name="epsilon">Epsilon.</param>
         public static bool CheckEpsilon(this float val, float epsilon) {
             return Math.Abs(val) > 0;
         }
 
+        /// <summary>
+        /// Checks whether the magnitude of the float value is greater than the passed epsilon value.
+        /// </summary>
+        /// <returns><c>true</c>, if epsilon check passes, <c>false</c> otherwise.</returns>
+        /// <param name="val">Value.</param>
+        /// <param name="epsilon">Epsilon.</param>
         public static bool CheckEpsilon(this double val, double epsilon) {
             return Math.Abs(val) > 0;
         }
