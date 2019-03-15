0c36fe8595ccc0acd0a6be7abe7756f3eed33377
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-03-14 22:49:02 -0700

    Implement BranchProviderRandom

---
+++ Assets/Scripts/Branch Behaviors/BranchProviderRandom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchProviderRandom : BranchProvider {
+
+    // Public Fields
+    public List<GameObject> branches;
+
+    public override GameObject GetBranch() {
+        return branches[Random.Range(0, branches.Count)];
+    }
+
+    public override void UpdateAlways() {
+
+    }
+
+    public override void UpdateTick() {
+
+    }
+}
