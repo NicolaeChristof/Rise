a282346f98cbc2f7d6eca2dde22de50addc705b0
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-04-06 11:04:42 -0700

    updated misletoe prefab

---
+++ Assets/Scripts/Obstacles and Enemies/MisletoeBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisletoeBehavior : MonoBehaviour {
+
+    // Public References
+
+    // Public Fields
+
+    // Private References
+
+    // Private Fields
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
