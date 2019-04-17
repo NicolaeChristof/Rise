46fefc1e88e934c5e8f5ee61c08f33b5212cdce6
AadamM <armohamm@ucsc.edu>
2019-04-17 15:46:38 -0700

    The leaves now move and rotate to the specific locations on the UI branch

--- Assets/Scripts/Branch Behaviors/UIBranchManager.cs
+++ Assets/Scripts/Branch Behaviors/UIBranchManager.cs
@@ -11,7 +11,7 @@ public class UIBranchManager : RiseBehavior {
     public TreeController treeController;
     public Image branch;
     public List<Image> leaves;
-    public Camera cam;
+    private Camera cam;
 
     // Internal Fields
     private static System.Action<MonoBehaviour> pauseComponent = (component) => { component.enabled = GameModel.paused; };
@@ -20,8 +20,11 @@ public class UIBranchManager : RiseBehavior {
 
     public Image moveableLeaf;
 
+    private BranchProvider currentProvider;
+
     void Start() {
         treeController.uiUpdateEvent += OnUpdateElement;
+        cam = GameObject.FindGameObjectWithTag("Squirrel Camera").GetComponent<Camera>();
     }
 
     void Update() {
@@ -38,6 +41,7 @@ public class UIBranchManager : RiseBehavior {
     }
 
     public void OnUpdateElement(BranchProvider provider) {
+        currentProvider = provider;
         branch.sprite = provider.branchSprite;
         int enabledSprites = Mathf.RoundToInt((provider.Sap / provider.maxSap) * leaves.Count);
         for (int index = 0; index < leaves.Count; index += 1) {
@@ -51,7 +55,17 @@ public class UIBranchManager : RiseBehavior {
         Vector3 uiPos = cam.WorldToScreenPoint(startPos);
         Image currentMoveableLeaf = GameObject.Instantiate(moveableLeaf, uiPos, Quaternion.identity, transform);
 
-        currentMoveableLeaf.transform.DOMove(transform.position, 2f, false).OnComplete(() => currentMoveableLeaf.enabled = false);
-        currentMoveableLeaf.transform.DOScale(transform.localScale * .3f, 2f);
+        int sap = (int)currentProvider.Sap - 1;
+
+        leaves[sap].enabled = false;
+
+        currentMoveableLeaf.transform.DOLocalRotate(leaves[sap].transform.localEulerAngles, 2f).OnComplete(() => leaves[sap].enabled = true);
+        currentMoveableLeaf.transform.DOMove(leaves[sap].transform.position, 2f, false).OnComplete(() => currentMoveableLeaf.enabled = false);
+        currentMoveableLeaf.transform.DOScale(.2f, 2f);
+    }
+
+    private void BranchFullyUpdated(Image currentLeaf, int currentSap) {
+        currentLeaf.enabled = false;
+        leaves[currentSap].enabled = true;
     }
 }
