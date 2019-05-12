e64122b95b92e08ac732cbad1eec1f5b4c6dee8d
Nicolae Christoffersen <nchrist1@ucsc.edu>
2019-05-12 12:09:36 -0700

    modified tree player

---
+++ Assets/Scripts/TreePlayerAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreePlayerAnimator : MonoBehaviour {
+
+    // Public References
+
+    // Public Fields
+
+    // Private References
+    private Transform _tree;
+
+    // Private Fields
+
+    // Start is called before the first frame update
+    void Start() {
+
+        _tree = transform.GetChild(0);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.Rotate(0.0f, 0.0f, 5.0f);
+        
+    }
+}
